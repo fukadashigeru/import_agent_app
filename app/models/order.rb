83cd5136d81ada5181968f752a521d3abf6c1888
fukadashigeru <sakusaku721@gmail.com>
2020-09-30 22:40:50 +0900

    ja.yml追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,12 @@
 class Order < ApplicationRecord
-  enum status: { before_order: 1, ordered: 2, buying: 3, shipped: 4 }
+  enum status: {
+    before_order: 1,
+    ordered: 2,
+    buying: 3,
+    shipped: 4
+  }
+
+  def status_i18n
+    I18n.t status, scope: %i[enum orders status]
+  end
 end
