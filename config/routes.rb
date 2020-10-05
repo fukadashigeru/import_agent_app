bcc4fa260e04fcdcec6c098968b5a717fd8332fa
fukadashigeru <sakusaku721@gmail.com>
2020-10-05 09:20:37 +0900

    orgs#index追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'orders/ordering_org_sides#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :orgs, only: %i[index]
   namespace :orders do
     resources :ordering_org_sides, only: %i[index]
   end
