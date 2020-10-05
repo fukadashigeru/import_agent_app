32068ead847ff91d63da5b29a773fee2f90a02c3
fukadashigeru <sakusaku721@gmail.com>
2020-10-05 09:54:12 +0900

    orgs#show追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :orgs, only: %i[index]
   namespace :orders do
-    resources :ordering_org_sides, only: %i[index]
+    resources :ordering_org_sides, only: %i[index, show]
   end
 end
