28398b41bbd8ede5f478c9eb37115fd1419de19f
fukadashigeru <sakusaku721@gmail.com>
2020-10-10 01:48:48 +0900

    会社の詳細画面修正

--- app/components/layout/navbar_component.rb
+++ app/components/layout/navbar_component.rb
@@ -1,6 +1,7 @@
 class Layout::NavbarComponent < ViewComponent::Base
   def initialize(org:, tab:)
-    @org = org
+    # TODO: orgのログイン・セッション回りを実装したら修正予定
+    @org = org ? org : Org.first
     @tab = tab
   end
 end
