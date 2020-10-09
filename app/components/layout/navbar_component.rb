19380e65e9a46998db3d228c2da009535084563a
fukadashigeru <sakusaku721@gmail.com>
2020-10-10 02:04:40 +0900

    注文一覧のタブをいい感じにする

--- app/components/layout/navbar_component.rb
+++ app/components/layout/navbar_component.rb
@@ -1,7 +1,8 @@
 class Layout::NavbarComponent < ViewComponent::Base
-  def initialize(org:, tab:)
+  def initialize(org:, tab: nil, active: nil)
     # TODO: orgのログイン・セッション回りを実装したら修正予定
     @org = org ? org : Org.first
     @tab = tab
+    @active = active
   end
 end
