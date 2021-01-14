5bc9a441e38187d79b2ca0e588be5683e9f3f123
fukadashigeru <sakusaku721@gmail.com>
2021-01-14 20:36:18 +0900

    rspec,factroy_botの設定追加

---
+++ spec/factories/org.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :org do
+    sequence(:name) { |n| "TEST_NAME#{n}"}
+    org_type { 0 }
+  end
+end
---
+++ spec/models/org_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe Org, type: :model do
+end
