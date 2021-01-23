5358c3b50553efd4edc847061a398853a6335afd
fukadashigeru <sakusaku721@gmail.com>
2021-01-23 12:27:46 +0900

    orgとorderの関連付けのオプション指定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
-  belongs_to :ordering_org, class_name: 'Org'
-  belongs_to :buying_org, class_name: 'Org'
+  belongs_to :ordering_org, class_name: 'Org', inverse_of: :orders_to_order
+  belongs_to :buying_org, class_name: 'Org', inverse_of: :orders_to_buy
   enum status: {
     before_order: 1,
     ordered: 2,
--- app/models/org.rb
+++ app/models/org.rb
@@ -1,4 +1,4 @@
 class Org < ApplicationRecord
-  has_many :orders_to_order, class_name: 'Order', foreign_key: :ordering_org_id
-  has_many :orders_to_buy, class_name: 'Order', foreign_key: :buying_org_id
+  has_many :orders_to_order, class_name: 'Order', foreign_key: :ordering_org_id, dependent: :restrict_with_error, inverse_of: :ordering_org
+  has_many :orders_to_buy, class_name: 'Order', foreign_key: :buying_org_id, dependent: :restrict_with_error, inverse_of: :buying_org
 end
