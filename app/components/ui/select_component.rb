c4d4d874026b87158dbb9e33268e02fa5dfaf39d
fukadashigeru <sakusaku721@gmail.com>
2021-01-24 13:33:31 +0900

    セレクトボックスのコンポーネント化

---
+++ app/components/ui/select_component.rb
@@ -0,0 +1,12 @@
+module Ui
+  class SelectComponent < ViewComponent::Base
+    def initialize(select_options, options = {}, form:, name:, **props)
+      super
+      @select_options = select_options
+      @options = options
+      @form = form
+      @name = name
+      @props = props
+    end
+  end
+end
