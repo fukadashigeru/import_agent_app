4be404e9168f36eedffb78938aa40b3fed39ec2c
fukadashigeru <sakusaku721@gmail.com>
2021-01-27 08:13:58 +0900

    fix rubocop

--- app/models/org.rb
+++ app/models/org.rb
@@ -1,6 +1,12 @@
 class Org < ApplicationRecord
-  has_many :orders_to_order, class_name: 'Order', foreign_key: :ordering_org_id, dependent: :restrict_with_error, inverse_of: :ordering_org
-  has_many :orders_to_buy, class_name: 'Order', foreign_key: :buying_org_id, dependent: :restrict_with_error, inverse_of: :buying_org
+  has_many :orders_to_order, class_name: 'Order',
+                             foreign_key: :ordering_org_id,
+                             dependent: :restrict_with_error,
+                             inverse_of: :ordering_org
+  has_many :orders_to_buy, class_name: 'Order',
+                           foreign_key: :buying_org_id,
+                           dependent: :restrict_with_error,
+                           inverse_of: :buying_org
   enum org_type: {
     ordering_org: 1,
     buying_org: 2
