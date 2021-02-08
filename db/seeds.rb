6eac7eaae43039de4bac9847719981c4c53c2e28
fukadashigeru <sakusaku721@gmail.com>
2021-02-08 22:13:53 +0900

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,9 +17,13 @@ Org.create(
 org_a = Org.find_by(name: '会社_a')
 org_b = Org.find_by(name: '会社_b')
 
-orders = Order.create(
+supplier_a = Supplier.create(org: org_a, shop_type: :default)
+Supplier.create(org: org_a, shop_type: :default)
+
+Order.create(
   [
     {
+      shop_type: :default,
       item_no: '21942661',
       trade_no: '97547563',
       title: '◯◯◯◯◯◯◯◯◯◯',
@@ -32,11 +36,13 @@ orders = Order.create(
       selling_unit_price: 10_000,
       information: '△△△',
       memo: 'XXX',
-      status: 1,
+      status: :before_order,
       ordering_org_id: org_a.id,
-      buying_org_id: org_b.id
+      buying_org_id: org_b.id,
+      supplier: supplier_a
     },
     {
+      shop_type: :default,
       item_no: '43042644',
       trade_no: '56654093',
       title: '◯◯◯◯◯◯◯◯◯◯',
@@ -49,11 +55,13 @@ orders = Order.create(
       selling_unit_price: 10_000,
       information: '△△△',
       memo: 'XXX',
-      status: 2,
+      status: :ordered,
       ordering_org_id: org_a.id,
-      buying_org_id: org_b.id
+      buying_org_id: org_b.id,
+      supplier: supplier_a
     },
     {
+      shop_type: :default,
       item_no: '81366219',
       trade_no: '46263602',
       title: '◯◯◯◯◯◯◯◯◯◯',
@@ -66,11 +74,13 @@ orders = Order.create(
       selling_unit_price: 10_000,
       information: '△△△',
       memo: 'XXX',
-      status: 3,
+      status: :buying,
       ordering_org_id: org_a.id,
-      buying_org_id: org_b.id
+      buying_org_id: org_b.id,
+      supplier: supplier_a
     },
     {
+      shop_type: :default,
       item_no: '60941267',
       trade_no: '76537895',
       title: '◯◯◯◯◯◯◯◯◯◯',
@@ -83,11 +93,13 @@ orders = Order.create(
       selling_unit_price: 10_000,
       information: '△△△',
       memo: 'XXX',
-      status: 4,
+      status: :shipped,
       ordering_org_id: org_a.id,
-      buying_org_id: org_b.id
+      buying_org_id: org_b.id,
+      supplier: supplier_a
     },
     {
+      shop_type: :default,
       item_no: '57200273',
       trade_no: '56939175',
       title: '◯◯◯◯◯◯◯◯◯◯',
@@ -100,11 +112,13 @@ orders = Order.create(
       selling_unit_price: 10_000,
       information: '△△△',
       memo: 'XXX',
-      status: 1,
+      status: :before_order,
       ordering_org_id: org_a.id,
-      buying_org_id: org_b.id
+      buying_org_id: org_b.id,
+      supplier: supplier_a
     },
     {
+      shop_type: :default,
       item_no: '56913758',
       trade_no: '83265169',
       title: '◯◯◯◯◯◯◯◯◯◯',
@@ -117,11 +131,13 @@ orders = Order.create(
       selling_unit_price: 10_000,
       information: '△△△',
       memo: 'XXX',
-      status: 2,
+      status: :ordered,
       ordering_org_id: org_a.id,
-      buying_org_id: org_b.id
+      buying_org_id: org_b.id,
+      supplier: supplier_a
     },
     {
+      shop_type: :default,
       item_no: '39821252',
       trade_no: '68545632',
       title: '◯◯◯◯◯◯◯◯◯◯',
@@ -134,11 +150,13 @@ orders = Order.create(
       selling_unit_price: 10_000,
       information: '△△△',
       memo: 'XXX',
-      status: 3,
+      status: :buying,
       ordering_org_id: org_a.id,
-      buying_org_id: org_b.id
+      buying_org_id: org_b.id,
+      supplier: supplier_a
     },
     {
+      shop_type: :default,
       item_no: '15498805',
       trade_no: '86154160',
       title: '◯◯◯◯◯◯◯◯◯◯',
@@ -151,11 +169,13 @@ orders = Order.create(
       selling_unit_price: 10_000,
       information: '△△△',
       memo: 'XXX',
-      status: 4,
+      status: :shipped,
       ordering_org_id: org_a.id,
-      buying_org_id: org_b.id
+      buying_org_id: org_b.id,
+      supplier: supplier_a
     },
     {
+      shop_type: :default,
       item_no: '72699536',
       trade_no: '73779350',
       title: '◯◯◯◯◯◯◯◯◯◯',
@@ -168,11 +188,13 @@ orders = Order.create(
       selling_unit_price: 10_000,
       information: '△△△',
       memo: 'XXX',
-      status: 1,
+      status: :before_order,
       ordering_org_id: org_a.id,
-      buying_org_id: org_b.id
+      buying_org_id: org_b.id,
+      supplier: supplier_a
     },
     {
+      shop_type: :default,
       item_no: '39959859',
       trade_no: '16022030',
       title: '◯◯◯◯◯◯◯◯◯◯',
@@ -185,11 +207,13 @@ orders = Order.create(
       selling_unit_price: 10_000,
       information: '△△△',
       memo: 'XXX',
-      status: 2,
+      status: :ordered,
       ordering_org_id: org_a.id,
-      buying_org_id: org_b.id
+      buying_org_id: org_b.id,
+      supplier: supplier_a
     },
     {
+      shop_type: :default,
       item_no: '83715346',
       trade_no: '48758961',
       title: '◯◯◯◯◯◯◯◯◯◯',
@@ -202,11 +226,13 @@ orders = Order.create(
       selling_unit_price: 10_000,
       information: '△△△',
       memo: 'XXX',
-      status: 3,
+      status: :buying,
       ordering_org_id: org_a.id,
-      buying_org_id: org_b.id
+      buying_org_id: org_b.id,
+      supplier: supplier_a
     },
     {
+      shop_type: :default,
       item_no: '42550477',
       trade_no: '94813841',
       title: '◯◯◯◯◯◯◯◯◯◯',
@@ -219,11 +245,13 @@ orders = Order.create(
       selling_unit_price: 10_000,
       information: '△△△',
       memo: 'XXX',
-      status: 4,
+      status: :shipped,
       ordering_org_id: org_a.id,
-      buying_org_id: org_b.id
+      buying_org_id: org_b.id,
+      supplier: supplier_a
     },
     {
+      shop_type: :default,
       item_no: '56606457',
       trade_no: '79330602',
       title: '◯◯◯◯◯◯◯◯◯◯',
@@ -236,18 +264,10 @@ orders = Order.create(
       selling_unit_price: 10_000,
       information: '△△△',
       memo: 'XXX',
-      status: 1,
+      status: :before_order,
       ordering_org_id: org_a.id,
-      buying_org_id: org_b.id
+      buying_org_id: org_b.id,
+      supplier: supplier_a
     }
   ]
 )
-
-supplier_url = org_a.supplier_urls.create(url: 'https://www.google.com/')
-
-orders.each do |order|
-  actual_unit = order.create_actual_unit
-  actual_unit.tap do |unit|
-    unit.actual_unit_urls.create(supplier_url: supplier_url)
-  end
-end
