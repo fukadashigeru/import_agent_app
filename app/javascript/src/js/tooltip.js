e3bd1d27e6a9156a50c02ffc8188c42c8b6e3aaf
fukadashigeru <sakusaku721@gmail.com>
2021-02-13 01:32:28 +0900

    tooltip修正

--- app/javascript/src/js/tooltip.js
+++ app/javascript/src/js/tooltip.js
@@ -1,12 +1,24 @@
 import tippy from 'tippy.js'
-import 'tippy.js/animations/perspective.css';
 import 'tippy.js/dist/tippy.css';
 
-document.addEventListener('mouseover', (e) => {
-  const mouseTarget = document.getElementById('questionAboutFirstPriority');
-  if (mouseTarget) {
-    tippy(mouseTarget, {
-      content: "第1希望の買付先にチェックを入れてください。",
-    });
+document.addEventListener('turbolinks:load', init)
+
+export function init () {
+  console.log('a')
+  tippy('[data-tooltip]', {
+    theme: 'tooltip',
+    content: (e) => e.getAttribute('data-tooltip')
+  })
+}
+
+document.addEventListener('mouseover', init_modal)
+
+function init_modal () {
+  const target = document.querySelector('[data-modal-tooltip]')
+  if (target) {
+    tippy(target, {
+      theme: 'tooltip',
+      content: (e) => e.getAttribute('data-modal-tooltip')
+    })
   }
-})
+}
