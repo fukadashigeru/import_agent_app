e75e4f1922aec5ae5526631f584154a85cc7c314
fukadashigeru <sakusaku721@gmail.com>
2021-04-11 01:46:09 +0900

    注文インポートまでの導線を修正

--- app/components/place_orders/navbar_component.rb
+++ app/components/place_orders/navbar_component.rb
@@ -1,20 +1,85 @@
 module PlaceOrders
   class NavbarComponent < ViewComponent::Base
-    def initialize(org:, active:)
+    def initialize(org:, import:, before_orders:, confirm:)
       super()
       @org = org
-      @active = active
+      @import = import
+      @before_orders = before_orders
+      @confirm = confirm
     end
 
-    def active_class
-      'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-500 text-sm font-medium
-      leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150
-      ease-in-out cursor-not-allowed pointer-events-none'
+    def import_border_color
+      case @import
+      when :done
+        'border-gray-700'
+      when :active
+        'border-indigo-600'
+      end
     end
 
-    def inactive_class
-      'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700
-      inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+    def import_text_color
+      case @import
+      when :done
+        'text-gray-700'
+      when :active
+        'text-indigo-600'
+      end
+    end
+
+    def before_orders_border_color
+      case @before_orders
+      when :done
+        'border-gray-700'
+      when :active
+        'border-indigo-600'
+      when :pending
+        if @org.orders_to_order.before_order.count.positive?
+          'border-gray-700'
+        else
+          'border-gray-300'
+        end
+      end
+    end
+
+    def before_orders_text_color
+      case @before_orders
+      when :done
+        'text-gray-700'
+      when :active
+        'text-indigo-600'
+      when :pending
+        if @org.orders_to_order.before_order.count.positive?
+          'text-gray-700'
+        else
+          'text-gray-300'
+        end
+      end
+    end
+
+    def import_pointer_events
+      'pointer-events-none' if current_page?([@org, :place_orders, :import])
+    end
+
+    def before_orders_pointer_events
+      'pointer-events-none' if @before_orders == :pending && @org.orders_to_order.before_order.blank?
+    end
+
+    def confirm_border_color
+      case @confirm
+      when :pending
+        'border-gray-300'
+      when :active
+        'border-indigo-600'
+      end
+    end
+
+    def confirm_text_color
+      case @confirm
+      when :pending
+        'text-gray-300'
+      when :active
+        'text-indigo-600'
+      end
     end
   end
 end
