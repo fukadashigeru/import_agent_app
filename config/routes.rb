6e6ea77f49d3af784a3cfe9a742e70b7b2aeacfd
fukadashigeru <sakusaku721@gmail.com>
2021-04-18 22:15:01 +0900

    add redirects_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'orgs#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # リダイレクトしてくれるエンドポイント
+  root 'redirects#index'
 
   # ログイン/セッション
   get    '/login',   to: 'sessions#new'
