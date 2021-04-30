c1c30f9ad41b014a0330c096b02a808af9e215ab
fukadashigeru <sakusaku721@gmail.com>
2021-04-30 13:33:53 +0900

    reduce argumen of Layout::NavbarComponent

--- app/components/layout/navbar_component.rb
+++ app/components/layout/navbar_component.rb
@@ -1,11 +1,10 @@
 module Layout
   class NavbarComponent < ViewComponent::Base
-    def initialize(org: nil, current_user: nil, tab: nil, active: nil, switch_company: false, company_detail: false)
+    def initialize(org: nil, current_user: nil, active: nil, switch_company: false, company_detail: false)
       # TODO: orgのログイン・セッション回りを実装したら修正予定
       super()
       @org = org
-      @current_user = current_user 
-      @tab = tab
+      @current_user = current_user
       @active = active
       @switch_company = switch_company
       @company_detail = company_detail
